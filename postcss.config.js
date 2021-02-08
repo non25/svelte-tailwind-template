4b2bfb515fe838918300c8cb528877fd7c21ea63
Non <19non91@gmail.com>
2021-02-08 16:30:33 +0200

    First attempt at something ?

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,27 @@
+const postcssImport = require('postcss-import');
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const prod = process.env.NODE_ENV === 'production';
+
+const configuredAutoprefixer = autoprefixer({
+  // config here
+});
+
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
-  ]
-}
+    tailwindcss,
+    postcssImport,
+    prod && purgecss({
+      content: [
+        './src/**/*.js',
+        './src/**/*.svelte',
+      ],
+    }),
+    configuredAutoprefixer,
+    prod && cssnano,
+  ].filter(Boolean),
+  pluginsLight: [postcssImport, configuredAutoprefixer]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const PostCSSAssetsPlugin = require('postcss-assets-webpack-plugin');
 const sveltePreprocess = require('svelte-preprocess');
 const path = require('path');
+
+const postcss = require('postcss');
+const postcssConfig = require('./postcss.config.js');
+const { pluginsLight } = postcssConfig;
+const postcssImport = require('postcss-import')
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
@@ -36,7 +41,21 @@ module.exports = {
               emitCss: prod,
               hotReload: !prod,
               preprocess: sveltePreprocess({
-                postcss: true
+                // inline imports in any style tag
+                defaults: { style: 'style' },
+                style({ content, filename }) {
+                  return postcss(prod ? [postcssImport] : pluginsLight)
+                    .process(content, { from: filename })
+                    .then(({ css: code, map }) => ({ code, map }));
+                },
+
+                // batch postcss stuff in production mode, but inline imports
+                // before they hit emitCss stage where they won't work properly
+                postcss({ content, filename }) {
+                  return postcss(prod ? [postcssImport] : postcssConfig.plugins)
+                    .process(content, { from: filename })
+                    .then(({ css: code, map }) => ({ code, map }));
+                }
               })
             }
           }
@@ -44,8 +63,27 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        // just load svelte component css without processing, it's already
+        // autoprefixed and has its imports inlined through svelte-preprocess
+        include: /svelte.\d+.css/,
         use: [
           MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /svelte.\d+.css/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            // for other css files, process with autoprefixer and inline
+            // imports, but leave autoprefixer for batch mode in production
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: { plugins: prod ? [postcssImport] : pluginsLight }
+            }
+          },
           'css-loader'
         ]
       },
@@ -77,14 +115,10 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css'
-    })
+    }),
+    // batch css processing in production
+    ...(prod ? [new PostCSSAssetsPlugin({ plugins: postcssConfig })] : [])
   ],
-  optimization: {
-    minimizer: [
-      `...`,
-      new CssMinimizerPlugin()
-    ],
-  },
   devtool: prod ? false : 'source-map',
   devServer: {
     hot: true,
